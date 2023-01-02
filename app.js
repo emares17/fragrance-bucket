90ddcbcfbbfefed8a48697eb17b9f31166383cdd
Esteban Mares <estebanmares17@gmail.com>
2023-01-02 14:41:48 -0800

    started intial routes

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const expressLayouts = require('express-ejs-layouts');
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'EJS');
+
+// Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/user'));
 
 app.listen(PORT, console.log(`Server has started on port ${PORT}`));
