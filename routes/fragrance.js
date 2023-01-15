84fcdbd7b08b4b8bc71e4149af1bc54d35387183
Esteban Mares <estebanmares17@gmail.com>
2023-01-15 14:36:07 -0800

    basic functionality completed, need styles and bug fixes

--- routes/fragrance.js
+++ routes/fragrance.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
 const Fragrance = require('../models/Fragrance');
 
-router.get('/add', (req, res) => res.render('add'));
+// Get add page
+router.get('/add', ensureAuth,(req, res) => res.render('add'));
+
+// Create post
+router.post('/', ensureAuth, (req, res) => {
+    req.body.user = req.user.id;
+    Fragrance.create(req.body)
+    .then(() => {
+        res.redirect('/dashboard');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+// Show all post to dashboard
+router.get('/', ensureAuth, (req, res) => {
+    Story.find({ status: 'public' }).lean()
+        .populate('user')
+        .sort({ date: 'desc' })
+        .then((fragrance) => {
+            res.render('index', {
+                fragrance
+            });
+    }).catch(err => console.log(err))
+});
+
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
+const Fragrance = require('../models/Fragrance')
+
 
 // Home page
 router.get('/', (req, res) => res.render('home'));
+
 // Dashboard
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuth, (req, res) => {
+    Fragrance.find({ user: req.user.id }).lean().then((fragrance) => {
+        res.render('dashboard', {
+            name: req.user.name, fragrance
+        });
+    }).catch(err => console.log(err));
+});
 
 module.exports = router
