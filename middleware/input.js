89e5dadf0581cc67d551739ebe9294a30a724a2c
Esteban Mares <estebanmares17@gmail.com>
2023-01-25 18:57:05 -0800

    started joi validation implementation

--- middleware/input.js
+++ middleware/input.js
@@ -1,23 +1,45 @@
+const Joi = require('joi');
+
 module.exports = {
     validateInput: (req, res, next) => {
         const { name, email, password, password2 } = req.body;
         let errors = [];
-    
-        // Check that all fields are filled in
-        if (!name || !email || !password || !password2) {
-            errors.push({ msg: 'Please fill in all fields' });
+
+        const schema = Joi.object({
+            name: Joi.string().when('email', {
+                is: Joi.string().empty(),
+                then: Joi.string().required().messages({
+                    'string.empty': 'Please fill in all fields',
+                    'any.required': 'Please fill in all fields'
+                }),
+                otherwise: Joi.string()
+            }),
+            email: Joi.string().when('password', {
+                is: Joi.string().empty(),
+                then: Joi.string().required().messages({
+                    'string.empty': 'Please fill in all fields',
+                    'any.required': 'Please fill in all fields'
+                }),
+                otherwise: Joi.string().email()
+            }),
+            password: Joi.string().min(6).required().messages({
+                'string.min': 'Password should be at least 6 characters long',
+                'any.required': 'Please fill in all fields'
+            }),
+            password2: Joi.string().valid(Joi.ref('password')).messages({
+                'any.only': 'Passwords do not match'
+            })
+        
+        });
+
+        const { error } = schema.validate({ name, email, password, password2 }, {abortEarly: false});
+
+        if(error) {
+            error.details.forEach(err => {
+                errors.push({ msg: err.message });
+            });
         }
-    
-        // Check passwords match
-        if (password !== password2) {
-            errors.push({ msg: 'Passwords do not match' });
-        }
-    
-        // Check password length
-        if (password.length < 6) {
-            errors.push({ msg: 'Password should be at least 6 characters' });
-        }
-    
+
         if (errors.length > 0) {
             res.render('register', {
                 errors,
@@ -31,3 +53,6 @@ module.exports = {
         }
     }
 };
+
+
+
